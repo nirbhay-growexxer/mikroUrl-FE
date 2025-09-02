e553b8dfb86e260486ddeed7b3c8286e3ab0119e
nirbhay-growexxer <nirbhay.parmar@growexx.com>
2025-09-03 01:03:12 +0530

    🐛 fix: added fix for ngrok header

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,6 +6,7 @@ const apiClient = axios.create({
   baseURL: (import.meta as any).env.VITE_API_URL || '',
   headers: {
     'Content-Type': 'application/json',
+    'ngrok-skip-browser-warning': '69420',
   },
 });
 
